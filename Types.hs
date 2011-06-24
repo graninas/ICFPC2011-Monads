f72cff7cd2cd3a3e5c2412def39b3221853d24be
Granin Alexandr <graninas@gmail.com>
2011-06-24 15:20:57 +0900

    - Улучшения, новые функции.

--- Types.hs
+++ Types.hs
@@ -29,9 +29,14 @@ data GameData = GD {propSlots :: Slots,
 	deriving (Eq, Show, Read)
 
 type GS a = (MST.StateT GameData Maybe a)
-	
+
+type CardFunction = [Maybe Function] -> GS (Maybe Function)
+
 defaultSlot = Slot 10000 I
 defaultSlots = zip [0..255] (replicate 256 defaultSlot)
+
+isSlotAlive (Slot v _) = v > 0
+
 
 dummyGameData = GD {propSlots = M.fromList defaultSlots,
 					opSlots   = M.fromList defaultSlots,
@@ -41,28 +46,39 @@ currentPlayerSlots (GD slots _ Player0) = slots
 currentPlayerSlots (GD _ slots Player1) = slots
 
 
-field :: Int -> Slots -> Maybe Function
-field i slots = do
+field :: Int -> Slots -> Bool -> Maybe Function
+field i slots isAlive = do
 			case M.lookup i slots of
-				Just (Slot v field) -> Just field
+				Just sl@(Slot v field) -> case isSlotAlive sl == isAlive of
+											True -> Just field
+											False -> Nothing
 				Nothing -> Nothing
 
-fZero :: [Maybe Function] -> GS (Maybe Function)
+fZero :: CardFunction
 fZero [] = return $ Just $ FValue 0
 fZero _  = return $ Nothing
 
-fI :: [Maybe Function] -> GS (Maybe Function)
+fI :: CardFunction
 fI (Just func:[]) = return $ Just $ func
 fI _ = return Nothing
 
-fGet :: [Maybe Function] -> GS (Maybe Function)
+fSucc :: CardFunction
+fSucc (Just (FValue x):[]) = return $ Just $ FValue (x + 1)
+fSucc _ = return Nothing
+
+fDbl :: CardFunction
+fDbl (Just (FValue x):[]) = return $ Just $ FValue (x * 2)
+fDbl _ = return Nothing
+
+fGet :: CardFunction
 fGet (Just (FValue i):[]) =
 		do
 			gd <- MST.get
-			return $ field i (currentPlayerSlots gd)
+			return $ field i (currentPlayerSlots gd) True
 fGet _ = return Nothing
+
 
 test1 = do
 			x <- fZero []
 			fI [x]
-test2 = [fI, fZero, fGet]
+test2 = [fI, fZero, fGet, fSucc]
