79fdb29ca9f1d747db98e17bcd5d6273d7787760
Granin Alexandr <graninas@gmail.com>
2011-06-24 16:39:45 +0900

    - Добавлена сложная функция inc.

--- Types.hs
+++ Types.hs
@@ -45,10 +45,25 @@ currentPlayerSlots (GD _ slots Player1) = slots
 field :: Int -> Slots -> Bool -> Maybe Function
 field i slots isAlive = do
 			case M.lookup i slots of
-				Just sl@(Slot v field) -> case isSlotAlive sl == isAlive of
+				Just sl@(Slot _ field) -> case isSlotAlive sl == isAlive of
 											True -> Just field
 											False -> Nothing
 				Nothing -> Nothing
+
+vitality :: Int -> Slots -> Bool -> Maybe Function
+vitality i slots isAlive = do
+			case M.lookup i slots of
+				Just sl@(Slot v _) -> case isSlotAlive sl == isAlive of
+											True -> Just $ FValue v
+											False -> Nothing
+				Nothing -> Nothing
+
+setSlotVitality :: Int -> Function -> GameData -> Maybe GameData
+setSlotVitality i (FValue newVit) gd@(GD slots _ _) =
+		case M.lookup i slots of
+			Just (Slot _ f) -> let updatedSl =  M.update (\x -> Just (Slot newVit f)) i slots in
+				Just $ gd {propSlots = updatedSl}
+			Nothing -> Nothing
 
 fZero :: [Maybe Function] -> GS (Maybe Function)
 fZero [] = return $ Just $ FValue 0
@@ -86,6 +101,19 @@ fS (Just (FFunc f):Just (FFunc g):x:[]) = do
 						Just (FFunc h') -> h' [y]
 						Nothing -> return Nothing
 
+fK ::  [Maybe Function] -> GS (Maybe Function)
+fK (x:_:[]) = return x
+
+fInc (Just (FValue i):[]) = do
+				gd <- MST.get
+				let oldVit = vitality i (currentPlayerSlots gd) True
+				let maybeNewGD = oldVit >>= (\(FValue v) -> if v < 65535 then Just $ FValue $ v + 1 else Just $ FValue v) >>= (\y -> setSlotVitality i y gd)
+				case maybeNewGD of
+					Just newGD -> do
+									MST.put newGD
+									x <- fI []
+									return x
+					Nothing -> return Nothing
 
 test1 = do
 			x <- fZero []
